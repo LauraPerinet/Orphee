69b8ce935b36a081b687719efefa92e4c3193563
renadr <ladrien2@gmail.com>
2018-05-11 00:29:04 +0200

    restore the JS to display connection and subscribtion popup

--- site/application/views/templates/footer.php
+++ site/application/views/templates/footer.php
@@ -9,8 +9,30 @@
     <!-- Bootstrap core JavaScript -->
     <script src="<?php echo base_url(); ?>styles/vendor/jquery/jquery.min.js"></script>
     <script src="<?php echo base_url(); ?>styles/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-   
+    <script>
+        new Vue({
+            el: '#orphee',
+            data: {
+            },
+            methods: {
+                closePopUp: function () {
+                    document.getElementsByClassName('show')[0].classList.add("hide");
+                    document.getElementsByClassName('show')[0].classList.remove("show");
+                    document.getElementsByClassName('show')[0].classList.add("hide");
+                    document.getElementsByClassName('show')[0].classList.remove("show");
+                },
+                openPopUp: function (className) {
+                    if(document.getElementsByClassName('show')[0] != undefined) {
+                        this.closePopUp();
+                    }
+                    document.getElementsByClassName(className)[0].classList.add("show");
+                    document.getElementsByClassName(className)[0].classList.remove("hide");
+                    document.getElementsByClassName('black-bg-opacity')[0].classList.add("show");
+                    document.getElementsByClassName('black-bg-opacity')[0].classList.remove("hide");
+                }
+            }
+        });
+    </script>
 
   </body>
 
