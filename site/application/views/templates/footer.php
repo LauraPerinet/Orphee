4699691c52ffd7525a0c0d0deb9bec78ecdd7118
JKoehren <koe.jimmy@gmail.com>
2018-05-11 02:39:57 +0200

    merge drag&drop

--- site/application/views/templates/footer.php
+++ site/application/views/templates/footer.php
@@ -32,6 +32,60 @@
                 }
             }
         });
+        
+        //Drag&Drop
+        
+        $(function(){
+            if ($("#completeOuvrage").length) {
+                $.getScript(
+                    //Import jQuery UI
+                    "<?php echo base_url(); ?>styles/vendor/jquery/jquery-ui.min.js", 
+                    function(){
+                        
+                        var out;
+                        var bb = $(".book .bloc");
+                        bb.css("display","flex");
+                        var h = bb.height();
+                        bb.css({
+                            height: h + "px",
+                            display: "auto"
+                        });
+                        $(".book").css("background","none");
+                        
+                        //Fiche
+                        
+                        $(".allSheets.drop").sortable({
+                            items: ".draggable",
+                        }).disableSelection();
+                        $(".book").droppable({
+                            
+                            //Ajout d'une fiche dans l'ouvrage
+                            
+                            drop: function(event, ui) { 
+                                if ($(ui.draggable).parents(".allSheets").length) window.open($(ui.draggable).find("a").prop("href"),"_self",false);
+                            }
+                        })
+                        
+                        //Ouvrage
+                        
+                        $(".book.drop").sortable({
+                            items: ".draggable",
+                            
+                            //Décalage image
+                            
+                            update: function(event, ui) {console.log("decalage image à save")},
+                            
+                            //Suppression d'une fiche dans l'ouvrage
+                            
+                            out: function(event, ui) {out = event.timeStamp},
+                            stop: function(event, ui) {
+                                if (out != event.timeStamp) window.open($(ui.item).find(".delete").parent().prop("href"),"_self",false);
+                            },
+                        }).disableSelection();
+                    }
+                );
+            };
+        });
     </script>
 
   </body>
