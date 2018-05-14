315b0648065719e118dc29d21065ae06c4891924
Adrien Lemaire <ladrien2@gmail.com>
2018-05-15 00:19:27 +0200

    Make the slider works

--- site/application/views/pages/informationSheetSlider.php
+++ site/application/views/pages/informationSheetSlider.php
@@ -7,15 +7,22 @@
     </div>
     <div class="row">
         <div class="col-12">
-            <div id="imageCarousel" class="carousel slide" data-interval="2000" data-ride="carousel" data-pause="hover" data-wrap="true">
-                <ol class="carousel-indicators">
-                    <li data-target="#imageCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#imageCarousel" data-slide-to="1"></li>
-                    <li data-target="#imageCarousel" data-slide-to="2"></li>
-                </ol>
+            <div id="imageCarousel" class="carousel slide" data-interval="false" data-ride="carousel" data-wrap="true">
+<!--                <ol class="carousel-indicators">-->
+<!--                    --><?php
+//                    for($i=0;$i<count($InformationSheets);$i++) {?>
+<!--                        <li data-target="#imageCarousel" data-slide-to="--><?php //echo $i; ?><!--" class="--><?php //if($i==0) echo "active";?><!--"></li>-->
+<!--                    --><?php
+//                    }
+//                    ?>
+<!--                </ol>-->
                     <div class="carousel-inner">
+                        <?php
+                            $firstOcc = true;
+                        ?>
                     <?php foreach($InformationSheets as $sheetPage):?>
-                        <div class="item active">
+                        <div class="carousel-item <?php if($firstOcc) echo "active" ?>">
+                            <?php $firstOcc=false;?>
                         <?php foreach($sheetPage as $sheetRow):?>
                             <div class="row">
                             <?php foreach($sheetRow as $sheet):?>
@@ -127,14 +134,24 @@
                         </div>
                     <?php endforeach;?>
                     </div>
-                    <a href="#imageCarousel" class="carousel-control left" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left"></span>
-                    </a>
-                    <a href="#imageCarousel" class="carousel-control right" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right"></span>
-                    </a>
+                <a class="carousel-control-prev" href="#imageCarousel" role="button" data-slide="prev" style="margin-left: -100px">
+                    <span class="carousel-control-prev-icon" aria-hidden="true" style="color: #0b2e13"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#imageCarousel" role="button" data-slide="next" style="margin-right: -100px">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
                 </div>
 
+            </div>
+            <div class="col-12" style="text-align: center;">
+                <?php
+                for($i=0;$i<count($InformationSheets);$i++) {?>
+                    <a href="#" class="orphee-link" data-target="#imageCarousel" data-slide-to="<?php echo $i;?>"><?php echo $i+1;?></a>
+                <?php
+                }
+                ?>
             </div>
         </div>
 
