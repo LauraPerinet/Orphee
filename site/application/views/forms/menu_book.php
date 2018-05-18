b2dd47121e9457d53e52abdf72e98233d98120b6
LauraPerinet <lpmarquet@gmail.com>
2018-05-18 12:11:12 +0200

    correction style boutons

--- site/application/views/forms/menu_book.php
+++ site/application/views/forms/menu_book.php
@@ -1,6 +1,4 @@
 	<div id="menu_book">
-		<div class="btnNew">
-			<a href="<?php echo site_url("ouvrage/export/".$book->ID); ?>">Assemblage</a>
-		</div>
+			<a href="<?php echo site_url("ouvrage/export/".$book->ID); ?>"><button class="orphee-btn">Assemblage</button></a>
 	</div>
 </div>
